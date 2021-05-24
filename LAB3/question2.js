d08098f2ba682e8e6a23e1035a3748d40655cf3f
satsbeha <79467896+satsbeha@users.noreply.github.com>
2021-05-23 20:43:23 -0700

    lab3

---
+++ LAB3/question2.js
@@ -0,0 +1,11 @@
+var myself = {
+    name: "solomon",
+    hobbies: ["violine", "cooking"],
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+myself.deposit(3000);
+console.log("Homework 3 question number 2");
+console.log(myself);
